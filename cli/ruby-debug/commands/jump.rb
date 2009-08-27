515351068f08a429d8d8e7c3fbb2ceabbbd6695d
Mark Moseley <mark@fast-software.com>
2009-08-27 15:26:46 -0700

    jump command updates

--- cli/ruby-debug/commands/jump.rb
+++ cli/ruby-debug/commands/jump.rb
@@ -4,20 +4,34 @@ module Debugger
   class JumpCommand < Command
     self.allow_in_control = true
 
+    def numeric?(object)
+      true if Float(object) rescue false
+    end
+
     def regexp
       / ^\s*
          (?:jump) \s*
+         (?:\s+(\S+))?\s*
          (?:\s+(\S+))?\s*
          $
       /ix
     end
 
     def execute
+      stack_size = Debugger.current_context.stack_size
+      if !numeric?(@match[1])
+        puts "Bad line number: " + @match[1]
+        return false
+      end
       line = @match[1].to_i
-      if Debugger.current_context.jump line
-        CommandProcessor.print_location_and_text(@state.context.frame_file(0), line)
+      file = @match[2]
+      file = @state.context.frame_file(file.to_i) if numeric?(file)
+      file = @state.context.frame_file(0) if !file
+      if Debugger.current_context.jump(line, file)
+        @state.line = line if stack_size == Debugger.current_context.stack_size
+        CommandProcessor.print_location_and_text(file, line)
       else
-        puts "failed"
+        puts "failed; PC unchanged"
       end
     end
 
